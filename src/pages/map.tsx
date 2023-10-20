a32a9c9387b2e2fed9aa7465d0e4990482c7291f
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-10-19 17:30:04 -0700

    [add]: markers and route

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -14,20 +14,94 @@ export default function Map() {
     loader.load().then(async () => {
       const google = window.google;
       const mapElement = document.getElementById("map");
-      
+
       if (mapElement) {
         map = new google.maps.Map(mapElement, {
           center: {
             lat: 37.722169, 
             lng: -122.478443,
           },
-          zoom: 17,
-          mapTypeControl: true,
-      mapTypeControlOptions: {
-        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-        mapTypeIds: ["roadmap", "terrain"],
-      },
-      
+          zoom: 16, // Adjust the zoom level.
+          mapTypeControl: false, // Remove the map type control.
+          styles: [
+            {
+              featureType: "poi",
+              elementType: "labels",
+              stylers: [{ visibility: "off" }], // Hide labels for points of interest.
+            },
+            {
+              featureType: "poi",
+              elementType: "geometry",
+              stylers: [{ visibility: "off" }], // Hide points of interest.
+            },
+            {
+              featureType: "road",
+              elementType: "labels",
+              stylers: [{ visibility: "on" }], // Show road labels.
+            },
+            {
+              featureType: "road",
+              elementType: "geometry",
+              stylers: [
+                { visibility: "on" }, // Show roads.
+                { color: "#dcdcdc" }, // Lighten road color.
+              ],
+            },
+            {
+              featureType: "landscape",
+              elementType: "geometry",
+              stylers: [
+                { visibility: "on" },
+                { color: "#f2f2f2" },
+              ],
+            },
+            {
+              featureType: "landscape.man_made",
+              elementType: "geometry",
+              stylers: [
+                { visibility: "on" }, // Show human-made landscape.
+              ],
+            },
+          ],
+        });
+
+        // Add markers for the start and end points
+        const startMarker = new google.maps.Marker({
+          position: {
+            lat: 37.72190204203244,
+            lng: -122.47684565585695,
+          },
+          map: map,
+          title: 'Start Point',
+        });
+
+        const endMarker = new google.maps.Marker({
+          position: {
+            lat: 37.72302760931965,
+            lng: -122.48173155228987,
+          },
+          map: map,
+          title: 'End Point',
+        });
+
+        // Define the route
+        const directionsService = new google.maps.DirectionsService();
+        const directionsRenderer = new google.maps.DirectionsRenderer({
+          map: map,
+        });
+
+        const request = {
+          origin: new google.maps.LatLng(37.72190204203244, -122.47684565585695),
+          destination: new google.maps.LatLng(37.72302760931965, -122.48173155228987),
+          travelMode: google.maps.TravelMode.WALKING,
+        };
+
+        directionsService.route(request, (result, status) => {
+          if (status === google.maps.DirectionsStatus.OK) {
+            directionsRenderer.setDirections(result);
+          } else {
+            console.error('Directions request failed with status:', status);
+          }
         });
       }
     });
