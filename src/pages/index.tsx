505827c3958aee3be090a955d6edf65f04625d67
diego-rey-2003 <diego.rey2300@gmail.com>
2023-10-26 12:20:01 -0700

    Set the backgroundalong with "Are you lost? it happens to the best of us"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,34 +2,41 @@ import React from 'react';
 
 export default function Home() {
   const shouldSetBackground = true; 
+  
 
-  const dynamicBackgroundClass = shouldSetBackground ? 'bg-purple-500' : 'bg-purple-500';
-
+  const dynamicBackgroundClass = shouldSetBackground ? 'bg-purple-#8b5cf6' : 'bg-purple-#8b5cf6';
+  
   return (
-    <main className="min-h-screen flex items-center justify-center bg-purple">
+    <main className="min-h-screen flex items-center justify-center">
       <div id="LogInPageRoot" className="flex flex-col items-center">
         <div
           id="background"
-          className={`bg-cover bg-center bg-no-repeat h-screen w-full relative ${dynamicBackgroundClass}`}
-          style={{ backgroundImage: `url('./assets/background.png')` }}
+          className={`bg-cover bg-center bg-no-repeat h-full w-full relative ${dynamicBackgroundClass}`}
+          style={{ backgroundImage: `url('./assets/background.png')`, width: '100vw', height: '60vw' }}
         >
-          <div className="text-6xl font-ramabhadra text-white w-1/2 text-center mt-48">
-            Are you lost? It happens to the best of us.
+          <div className="text-8xl font-ramabhadra text-white w-3/4 text-left mt-48 ml-40 ">
+            Are you lost? 
           </div>
-          <div className="flex flex-col items-center mt-4 space-y-12">
-            <div className="flex items-center space-x-8">
-              <div className="bg-cover bg-center bg-no-repeat w-32 h-32"></div>
-              <div className="text-5xl font-roboto-mono font-medium text-orange mt-4">
+          <div className="text-8xl font-ramabhadra text-white w-3/4 text-left  ml-40 ">
+             It happens to 
+          </div>
+          <div className="text-8xl font-ramabhadra text-white w-3/4 text-left  ml-40 ">
+             the best of us.
+          </div>
+          <div>
+            <div>
+              <div className="text-5xl font-roboto-mono font-medium text-[#ff9d5b]  w-3/4 text-right my-50 ml-40">
                 SF_MAPS
               </div>
             </div>
-            <div className="w-[480px] h-40 bg-white rounded-lg flex items-center justify-end px-8">
-              <img src="" alt="Google Logo" className="w-24 h-16 absolute top-1 left-2" />
-              <div className="text-3xl font-open-sans font-bold text-center relative">
+            <div>
+              <img src="./public/assets/image1.png" alt="Google Logo"
+              className= "w-24 h-16 text-right ml-9" />
+              <div className="text-3xl font-open-sans font-bold text-center">
                 Continue with Google.
               </div>
             </div>
-            <div className="text-xl font-ramabhadra text-[#4ae7ff] ml-6 w-3/5">
+            <div className="text-xl font-ramabhadra text-[#4ae7ff]  w-3/5 text-right ml-96 h-full">
               Continue as guest.
             </div>
           </div>
