2ad57cd8c946111adbabe4392d58591752d83d4c
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-10-26 13:04:07 -0700

    [fix]: build error

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -4,13 +4,6 @@ export default function home() {
   return (
     <div>
       home
-      <style>
-        div {
-        width: auto;
-        height: 300px;
-        background-color: purple;
-        }
-      </style>
     </div>
   )
 }
