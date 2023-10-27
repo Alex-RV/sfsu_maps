13f0b83415250ffa3df4b4f1f25d6359661411a5
RussianGravy <77594379+RussianGravy@users.noreply.github.com>
2023-10-26 23:05:10 -0700

    added command bar

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function home() {
   return (
-    <div>
-      home
+    <div id="contol bar" className="w-full bg-violet-700">
+      <div id="logo and title" className="flex items-left">
+        <img style={{ backgroundImage: `url('./public/assets/gator_logo.png')`, width: '30px', height: '30px' }} className="ml-4"/>
+        <div id ="title" className="ml-4">SFSU_Maps</div>
+        </div>
     </div>
   )
 }
