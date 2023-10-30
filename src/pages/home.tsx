ab1b545f19a52b0d71a3e2d460bb09de74b902f3
RussianGravy <77594379+RussianGravy@users.noreply.github.com>
2023-10-30 11:01:27 -0700

    Resized some elements + made code more readable.
    - added comments. 
    - resized the destination input box.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -56,15 +56,16 @@ export default function home() {
       <div id="logo and title" className="flex items-left">
         <img src='./assets/gator_logo.png' style={{position: 'relative', left: '3.2vh', top: '0', width: '5vh', height: '5vh'}}  alt="gatorlogo"/>
         <div id ="title" style={{position: 'relative', left: '4vh', top: '0', fontSize:'3vh'}}>SF_Maps</div>
+    </div> {/* END */}
+    {/* home functions */}
+    <div id="home functions" className="w-full h-screen bg-violet-900 ">
+      {/* map space */}
+      <div id="map"
+      style={{borderRadius: '5vh', outlineStyle: 'solid', outlineWidth: '0.5vh', outlineColor: 'rgb(139 92 246)', position:'fixed', top:'7.5vh', left: '3vh',backgroundColor: 'white', width: '102vh', height: '90vh'}}>
       </div>
-      {/* home functions */}
-      <div id="home functions" className="w-full h-screen bg-violet-900 ">
-        <div id="map"
-        style={{borderRadius: '5vh', outlineStyle: 'solid', outlineWidth: '0.5vh', outlineColor: 'rgb(139 92 246)', position:'fixed', top:'7.5vh', left: '3vh',backgroundColor: 'white', width: '102vh', height: '90vh'}}>
-        </div>
-        {/* origin buttons */}
-        <div id="set_origin" className=""
-        style={{position:'fixed', top:'7.5vh', right: '3vh', width: '68.31vh', height: '34.5vh'}}>
+      {/* origin buttons */}
+      <div id="set_origin" className=""
+      style={{fontSize: '2.5vh', position:'fixed', top:'7.5vh', right: '3vh', width: '68.31vh', height: '34.5vh'}}>
           <p style={{marginBottom: '2.07vh'}}>Input your origin. Where are you now?</p>
           <div style={{borderRadius: '5vh', outlineStyle: 'solid', outlineWidth: '0.5vh', outlineColor: 'rgb(139 92 246)',backgroundColor: 'rgb(232, 218, 255)'}}>
           <input id='search origin' style={{width: '64.17vh', borderStyle: 'solid', borderColor: 'black', borderWidth: '0.15vh', color: 'black', marginLeft:'2.07vh', marginTop: '2.07vh', marginBottom: '2.07vh', borderRadius: '5vh'}}></input>
@@ -83,13 +84,13 @@ export default function home() {
             </button>
           </div>
         </div>
-        </div>
-        {/* destination buttons */}
-        <div id="set_destin" className=""
-        style={{position:'fixed', top:'50vh', right: '3vh', width: '74.25vh', height: '37.5vh'}}>
-          <p style={{marginBottom: '2.25vh'}}>Input your destination. Where are you going?</p>
+      </div>
+      {/* destination buttons */}
+      <div id="set_destin" className=""
+      style={{position:'fixed', top:'48vh', right: '3vh', width: '68.31vh', height: '34.5vh'}}>
+          <p style={{fontSize: '2.5vh', marginBottom: '2.25vh'}}>Input your destination. Where are you going?</p>
           <div style={{borderRadius: '5vh', outlineStyle: 'solid', outlineWidth: '0.5vh', outlineColor: 'rgb(139 92 246)',backgroundColor: 'rgb(232, 218, 255)'}}>
-          <input id='search origin' style={{width: '69.75vh', borderStyle: 'solid', borderColor: 'black', borderWidth: '0.15vh', color: 'black', marginLeft:'2.25vh', marginTop: '2.25vh', marginBottom: '3vh', borderRadius: '5vh'}}></input>
+          <input id='search origin' style={{width: '64.17vh', borderStyle: 'solid', borderColor: 'black', borderWidth: '0.15vh', color: 'black', marginLeft:'2.25vh', marginTop: '2.25vh', marginBottom: '3vh', borderRadius: '5vh'}}></input>
           <div id='buttons' style={{width:'fill', color: 'black'}}>
             <button id='mary_ward' onClick={() => searchLocation(destination, maryWard)} style={{margin: '0.2vh', color: 'black',}}>
               <img src='./assets/residential.png' style={{width: '20.01vh', height: '20.01vh', marginLeft:'2.07vh'}}></img>
@@ -105,8 +106,13 @@ export default function home() {
             </button>
           </div>
         </div>
-        </div>
       </div>
-    </div>
-  )
+      {/*Build Route button*/}
+      <button id='build-route button'
+      style={{position: 'fixed', right: '3vh', top: '91.07vh', textAlign: 'center', borderRadius: '5vh', width: '68.31vh', height: '5vh', backgroundColor: 'lightblue'}}>
+        build route
+      </button>
+    </div> {/* END home functions*/}
+  </div>
+)
 }
