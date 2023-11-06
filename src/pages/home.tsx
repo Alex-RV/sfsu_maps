a44323122d542c74faeca43f7c0dfde13d331eb7
Alex-RV <59423285+Alex-RV@users.noreply.github.com>
2023-11-06 15:08:55 -0800

    [add]: responsive, scrollable
    able to accept more than 3 elements

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -30,6 +30,7 @@ export default function Home() {
     const results = locationsData.filter((location) =>
       location.name.toLowerCase().includes(inputValue.toLowerCase())
     );
+    console.log(results)
     setSearchResultsDestination(results);
   };
 
@@ -43,62 +44,64 @@ export default function Home() {
   };
   return (
     <div>
-    <div id="control-bar" className="w-full fixed top-0 left-0 bg-purple-600 h-16 flex items-center">
+    <div id="control-bar" className="w-full flex flex-row bg-purple-600 h-16  items-center">
       <img src="./assets/gator_logo.png" className="w-16 h-16 ml-4" alt="gatorlogo" />
       <div id="title" className="text-3xl ml-4">SF_Maps</div>
     </div>
 
-    <div id="home-functions" className="w-full h-screen bg-violet-900" style={{ backgroundImage: `url('./assets/background.png')` }}>
+    <div id="home-functions" className="w-full h-screen bg-violet-900 flex lg:flex-row md:flex-row flex-col-reverse justify-between" style={{ backgroundImage: `url('./assets/background.png')` }}>
       {/* <img src="./assets/background.png" className="w-full h-full opacity-50" alt="background" /> */}
 
-      <div id="map" className="rounded-3xl border-2 border-purple-500 fixed top-20 left-4 bg-white w-96 h-4/5"></div>
+      <div id="map" className="rounded-3xl border-2 border-purple-500 mt-10 ml-10 w-2/3 bg-white"></div>
+      <div className='flex flex-col mt-5 mr-5 w-1/3 ml-5'>
+        <div id="set-origin" className=" flex flex-col my-7">
+          <p className="text-xl mb-4">Input your origin. Where are you now?</p>
+          <div className="rounded-3xl border-2 border-purple-500 bg-purple-200 p-4">
+            <input
+              id="search-origin"
+              className="w-full border border-black p-2 mb-4 rounded-3xl"
+              placeholder="Search origin"
+              onChange={searchLocationsOrigin}
+            />
+            <div className="flex flex-row overflow-x-scroll ">
+            {searchResultsOrigin.map((location: Location) => (
+              <button key={location.name} id={location.image} onClick={() => selectLocation(location, false)} className="flex w-32 items-center p-2 m-2 flex-col flex-shrink-0">
+                <Image width={500} height={500} src={`/assets/${location.image}`} alt={location.name} className="w-20 h-20 mr-2" />
+                {location.name}
+              </button>
+              ))}
+            </div>
+          </div>
+        </div>
 
-      <div id="set-origin" className="fixed top-20 right-4 w-96 h-96">
-        <p className="text-xl mb-4">Input your origin. Where are you now?</p>
-        <div className="rounded-3xl border-2 border-purple-500 bg-purple-200 p-4">
+        <div id="set-destination" className=" flex flex-col my-7">
+          <p className="text-xl mb-4">Input your destination. Where are you going?</p>
+          <div className="rounded-3xl border-2 border-purple-500 bg-purple-200 p-4">
           <input
-            id="search-origin"
-            className="w-full border border-black p-2 mb-4 rounded-3xl"
-            placeholder="Search origin"
-            onChange={searchLocationsOrigin}
-          />
-          <div className="flex flex-row">
-          {searchResultsOrigin.map((location: Location) => (
-            <button key={location.image} id={location.image} onClick={() => selectLocation(location, false)} className="flex items-center p-2 m-2 flex-col">
-              <Image width={300} height={300} src={`/assets/${location.image}`} alt={location.name} className="w-20 h-20 mr-2" />
-              {location.name}
-            </button>
-            ))}
+              id="search-dest"
+              className="w-full border border-black p-2 mb-4 rounded-3xl"
+              placeholder="Search origin"
+              onChange={searchLocationsDestination}
+            />
+            <div className="flex flex-row overflow-x-auto ">
+            {searchResultsDestination.map((location: Location) => (
+              <button key={location.name} id={location.image} onClick={() => selectLocation(location, false)} className="flex w-32 items-center p-2 m-2 flex-col flex-shrink-0">
+                <Image width={500} height={500} src={`/assets/${location.image}`} alt={location.name} className="w-20 h-20 mr-2" />
+                {location.name}
+              </button>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-
-      <div id="set-destination" className="fixed top-96 right-4 w-96 h-96">
-        <p className="text-xl mb-4">Input your destination. Where are you going?</p>
-        <div className="rounded-3xl border-2 border-purple-500 bg-purple-200 p-4">
-        <input
-            id="search-dest"
-            className="w-full border border-black p-2 mb-4 rounded-3xl"
-            placeholder="Search origin"
-            onChange={searchLocationsDestination}
-          />
-          <div className="flex flex-row">
-          {searchResultsDestination.map((location: Location) => (
-            <button key={location.image} id={location.image} onClick={() => selectLocation(location, false)} className="flex items-center p-2 m-2 flex-col">
-              <Image width={300} height={300} src={`/assets/${location.image}`} alt={location.name} className="w-20 h-20 mr-2" />
-              {location.name}
-            </button>
-            ))}
-          </div>
+        <div className='items-center justify-center flex'>
+        <button
+          id="build-route-button"
+          className="bg-blue-400 text-black  border-black border-2 rounded-3xl p-2  w-96"
+        >
+          Build Route
+        </button>
         </div>
-      </div>
-
-      <button
-        id="build-route-button"
-        className="bg-blue-400 text-black border border-black border-2 rounded-3xl p-2 absolute bottom-4 right-4 w-96"
-      >
-        Build Route
-      </button>
+    </div>
     </div>
     </div>
   );
