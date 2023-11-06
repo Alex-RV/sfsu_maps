765371a8b1b6b6711dcaf04da8f1f1a05157a5df
diego-rey-2003 <diego.rey2300@gmail.com>
2023-11-05 16:48:01 -0800

    started with authentication

--- src/pages/auth.ts
+++ src/pages/auth.ts
@@ -1,10 +1,10 @@
 import {  NextAuthOptions} from "next-auth";
 import { useSession } from "next-auth/react";
 import { Redirect } from "next";
-
+import GoogleProvider from "next-auth/providers/google";
 
 import CredentialsProvider  from "next-auth/providers/credentials";
-import GoogleProvider from "next-auth/providers/github" 
+
 import GithubProvider from "next-auth/providers/github"
 
 
@@ -12,6 +12,9 @@ import GithubProvider from "next-auth/providers/github"
 export const authConfig: NextAuthOptions = {
     providers:
 [
-
-]
+    GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID!,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+      }),
+],
 };
